844a6b098aee2f0e7e1d08e4a5b6498e467401d6
matemat13 <shrajtofle@gmail.com>
2014-02-13 10:42:05 +0100

    Added the C library variant
    With the C library, it is actually even a bit slower...

---
+++ Lua with static C module/Plugins/RasPiGPIO/CoreMessaging.lua
@@ -0,0 +1,32 @@
+-- Copyright (c) 2013-2014 Alexander Harkness & Tiger Wang
+ 
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the
+-- "Software"), to deal in the Software without restriction, including
+-- without limitation the rights to use, copy, modify, merge, publish,
+-- distribute, sublicense, and/or sell copies of the Software, and to
+-- permit persons to whom the Software is furnished to do so, subject to
+-- the following conditions:
+ 
+-- The above copyright notice and this permission notice shall be included
+-- in all copies or substantial portions of the Software.
+ 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+-- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+-- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+function SendMessage(a_Player, a_Message)
+a_Player:SendMessage(cChatColor.Yellow .. "[INFO] " .. cChatColor.White .. a_Message)
+end
+ 
+function SendMessageSuccess(a_Player, a_Message)
+a_Player:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. a_Message)
+end
+ 
+function SendMessageFailure(a_Player, a_Message)
+a_Player:SendMessage(cChatColor.Red .. "[INFO] " .. cChatColor.White .. a_Message)
+end
